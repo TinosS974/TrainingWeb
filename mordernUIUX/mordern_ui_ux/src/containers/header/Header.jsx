a6a2bfbf1ae70520235f2e47cd9ea123cb4e4588
TinosS974 <raphael.salaverria@epitech.eu>
2023-03-10 02:16:40 -0500

    Header fully responsive done

--- mordernUIUX/mordern_ui_ux/src/containers/header/Header.jsx
+++ mordernUIUX/mordern_ui_ux/src/containers/header/Header.jsx
@@ -17,9 +17,9 @@ function Header() {
           <img src={people} alt="people"/>
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className="gpt3_header-image">
-          <img src={ai} alt="ai"/>
-        </div>
+      </div>
+      <div className="gpt3_header-image">
+        <img src={ai} alt="ai"/>
       </div>
     </div>
   )
