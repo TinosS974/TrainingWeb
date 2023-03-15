1b00adaf3e83d67e24ac238c766eac59fe2e54d0
TinosS974 <raphael.salaverria@epitech.eu>
2023-03-15 05:20:53 -0400

    download section done

--- landing_nft_page/src/App.js
+++ landing_nft_page/src/App.js
@@ -32,7 +32,7 @@ const App = () => {
         banner="banner02"
       />
       <Download />
-
+      
       <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
         <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
         <span className="bold">TinosS</span>
